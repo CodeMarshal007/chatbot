c09c1f03fd949457ef0f0071d8a0c65a37020c75
Adeniyi A. Yusuf <princeaayusuf@gmail.com>
2023-03-16 01:37:28 +0100

    fix view bug

--- src/app.js
+++ src/app.js
@@ -39,6 +39,7 @@ app.use(sessionMiddleware);
 app.use(express.static(path.join(__dirname, "public")));
 
 app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
